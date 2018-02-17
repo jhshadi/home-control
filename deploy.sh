a9b68065bf6fc49279822b6e8daae28cb9aa69d5
Shlomi <shlomi@innovid.com>
2018-02-17 23:06:07 +0200

    update deploy script #1

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-PORT=8090
+PORT=7070
 DOCKER_IMAGE="jhshadi/home-control"
 CONTAINER_NAME="home-control"
 
+echo "Cleaning old '$CONTAINER_NAME' container"
 docker stop home-control
 docker rm home-control
 
+echo "Building '$DOCKER_IMAGE' image"
 docker build -t ${DOCKER_IMAGE} .
-docker run --net=host -e PORT='"'${PORT}'"' --name=${CONTAINER_NAME} ${DOCKER_IMAGE}
+
+echo "Starting '$CONTAINER_NAME' container"
+docker run -d --net=host -e PORT='"'${PORT}'"' --name=${CONTAINER_NAME} ${DOCKER_IMAGE}
